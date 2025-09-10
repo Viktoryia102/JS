d30b4d14d1d7c2e359b24dc622863289be8982fc
Twix <vvasaevic@gmail.com>
2025-09-10 22:41:52 +0200

    Update vashkevich_zewnetrzny.js

--- vashkevich_zewnetrzny.js
+++ vashkevich_zewnetrzny.js
@@ -1,2 +1,5 @@
+document.write("Mój numer w dzienniku to 27"+"<br>");
+document.write("Pierwszy wiersz to:"+"&nbsp;&nbsp;2"+"&nbsp;&nbsp;Nie"+"&nbsp;&nbsp;&nbsp;Tak"+"&nbsp;&nbsp;6"+"<br>");
+document.write("Drugi wiersz to:"+"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;16"+"&nbsp;&nbsp;&nbsp;Tak"+"&nbsp;&nbsp;&nbsp;Nie"+"&nbsp;&nbsp;6"+"<br>");
 document.write("ostatnia modyfikacja strony".fontcolor("silver").fontsize(6).strike()+"<br>"); 
 document.write(document.lastModified.fontsize(6).fontcolor("adua").bold()+"<br>"); 
